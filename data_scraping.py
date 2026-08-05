cdccd40efbb515ca1f1c71cddb84dadd74a5c0f5
KarolOszek <oszekkarol@gmail.com>
2026-08-05 15:05:02 +0200

    added changing html format to DataFrame and fixed missing column names

--- data_scraping.py
+++ data_scraping.py
@@ -4,6 +4,8 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.support.wait import WebDriverWait
 import time
+import pandas as pd
+
 
 chrome_options = Options()
 chrome_options.add_argument("--disable-notifications")
@@ -14,7 +16,7 @@ time.sleep(2)
 driver.find_element(By.CLASS_NAME, 'fc-button.fc-cta-consent.fc-primary-button').click()
 
 def scrape():
-    data_from_table = driver.find_elements(By.TAG_NAME, 'table')[1].text
+    data_from_table = driver.find_elements(By.TAG_NAME, 'table')[1].get_attribute('outerHTML')
     return data_from_table
 
 
@@ -31,3 +33,9 @@ for i in range(1, len(elements)):
     driver.get("https://gol.gg/teams/list/season-ALL/split-ALL/tournament-LPL%202026%20Split%203/")
 
 print(tables)
+dfs = []
+for table in tables:
+    table_html = pd.read_html(table)
+    table_html[0] = table_html[0].rename(columns={'Unnamed: 3':'Kills', 'Unnamed: 4': 'Gold/sec','Unnamed: 5':'Towers','Unnamed: 6':'Dragons', 'Unnamed: 7':'Kills/15min', 'Unnamed: 8':'Gold/sec/15min', 'Unnamed: 9':'Towers/15min', 'Unnamed: 10':'Dragons/15min' })
+    dfs.append(table_html[0])
+print(dfs)
