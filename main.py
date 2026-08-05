ac383cdc53b9aa1bfc2ffc3f7b5257250dab05a8
KarolOszek <oszekkarol@gmail.com>
2026-08-05 15:10:30 +0200

    removed side stats cause of its irrelevance for predicting

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ def get_match_history(df, team_a, team_b, N=20):
   team_b_matches = df[df['Team'] == team_b].tail(N)
   stats_cols = ['Kills', 'Gold/sec', 'Towers', 'Dragons',
                   'Kills/15 minute', 'Gold/sec/15 minute',
-                  'Towers/15 minute', 'Dragons/15 minute', 'Side']
+                  'Towers/15 minute', 'Dragons/15 minute']
   team_a_stats = team_a_matches[stats_cols].mean()
   team_b_stats = team_b_matches[stats_cols].mean()
 
@@ -51,10 +51,10 @@ for df in dfs:
   dfs_fixed.append(df_temp)
 
 df_all = pd.concat(dfs_fixed, ignore_index=True)
-df_all = df_all.drop(columns=['Score', 'Tournament', 'Week', 'Game'])
+print(df_all)
+df_all = df_all.drop(columns=['Side', 'Score', 'Tournament', 'Week', 'Game'])
 df_all = df_all.rename({'Kills.1':'Kills/15 minute', 'Golds.1':'Gold/sec/15 minute', 'Towers.1':'Towers/15 minute', 'Dragons.1':'Dragons/15 minute', 'Golds':'Gold/sec'})
 df_all['Result'] = df_all['Result'].map({'WIN':1, 'LOSS':0})
-df_all['Side'] = df_all['Side'].map({'Blue':1, 'Red':0})
 df_all = df_all.rename(columns={'Kills.1':'Kills/15 minute', 'Golds.1':'Gold/sec/15 minute', 'Towers.1':'Towers/15 minute', 'Dragons.1':'Dragons/15 minute', 'Golds':'Gold/sec'})
 
 for idx, row in df_all.iterrows():
